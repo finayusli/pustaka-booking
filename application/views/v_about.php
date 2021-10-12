8236c149ab2217667669bbee236f844ef830a08a
Finayusli Andriani Artatik <12200025@bsi.ac.id>
2021-10-12 20:08:47 +0700

    isi pesan

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1> 
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Cholifah </li>
+        <li>Nama Belakang : Saeaturrosidah </li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Desa Pomahan Kecamatan Baureno Kabupaten Bojonegoro</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Gresik</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Catur</li>
+    </ul>
+</section> 
