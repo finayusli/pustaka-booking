8bb34aed5c1ff4b78ea5cc339713f59d8809dd3c
Finayusli Andriani Artatik <12200025@bsi.ac.id>
2021-10-20 20:01:56 +0700

    Menambahkan Validasi Controller & View

--- application/views/v_about.php
+++ application/views/v_about.php
@@ -2,23 +2,23 @@
     <h1><?php echo $judul ?></h1> 
     <h4>Nama</h4>
     <ul type="disc">
-        <li>Nama Depan : Cholifah </li>
-        <li>Nama Belakang : Saeaturrosidah </li>
+        <li>Nama Depan : Finayusli </li>
+        <li>Nama Belakang : Andriani Artatik </li>
     </ul>
     <br>
     <h4>Alamat</h4>
     <ul type="none">
-        <li> Desa Pomahan Kecamatan Baureno Kabupaten Bojonegoro</li>
+        <li> Pantog Kulon, Banjaroyo, Kalibawang, Kulon Progo</li>
     </ul>
     
     <h4>Tempat Lahir</h4>
     <ul type="none">
-        <li>Gresik</li>
+        <li>Kulon Progo</li>
     </ul>
 
     <h4>Olah Raga Favorit</h4>
     <ul type="square">
-        <li>Bulutangkis</li>
-        <li>Catur</li>
+        <li>Volly</li>
+        <li>Berenang</li>
     </ul>
 </section> 
